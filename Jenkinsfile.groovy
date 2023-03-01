0461f9222711b05bca20d0261cb05b2c3ae8b3b6
IshaqMd <55760349+ishaqmdgcp@users.noreply.github.com>
2023-03-01 22:49:43 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ pipeline {
       	stage('Set creds') {
             steps {
               
-                sh 'echo $SVC_ACCOUNT_KEY | base64 -d > ./Jenkins/jenkins.json'
+                sh 'echo $SVC_ACCOUNT_KEY | base64 -d > jenkins.json'
 		            sh 'pwd' 
             }
         }
@@ -17,14 +17,13 @@ pipeline {
 	
 	stage('Auth-Project') {
 	 steps {
-		 dir('Jenkins')
-		 {
+		 
         sh 'gcloud auth activate-service-account jenkins@mi-dev-lab.iam.gserviceaccount.com --key-file=jenkins.json'
        sh 'gcloud config set project mi-dev-env'
 			 sh 'gcloud compute addresses list > addresses_ips.txt'
     }
     }
-	}
+	
  	 
 	stage('Run Script') {
 	 steps {
